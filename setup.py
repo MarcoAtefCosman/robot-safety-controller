a2838cb0e304e51a307e72eae96dc51b8d8dfea6
MarcoAtefCosman <marcoatefcosmangadallah@gmail.com>
2025-05-29 21:00:04 +0200

    Add config and launch file to the state machine implementation

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'robot_safety_controller'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), ['config/params.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,4 +28,5 @@ setup(
             'behavior_tree = robot_safety_controller.behavior_tree:main',
         ],
     },
+
 )
