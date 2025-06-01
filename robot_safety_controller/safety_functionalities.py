320ed9d9aedb989113b380e6698c6a9b2f2144bc
MarcoAtefCosman <marcoatefcosmangadallah@gmail.com>
2025-06-01 16:40:23 +0200

    Add the behavior tree implementation of the CPU temperature monitoring

--- robot_safety_controller/safety_functionalities.py
+++ robot_safety_controller/safety_functionalities.py
@@ -6,15 +6,15 @@ from std_msgs.msg import Float32
 from sensor_msgs.msg import LaserScan
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
-class Rotate(pt.behaviour.Behaviour):
+class RotateBattery(pt.behaviour.Behaviour):
     def __init__(self, name):
         super().__init__(name)
-        self.cmd_pub = None
 
     def setup(self, **kwargs):
         self.node = kwargs['node']
-        self.cmd_pub = self.node.create_publisher(Twist, '/cmd_vel', 10)
         self.rotation_speed = self.node.get_parameter('recovery.rotation_speed').value
+
+        self.cmd_pub = self.node.create_publisher(Twist, '/cmd_vel', 10)
 
         return True
 
@@ -32,6 +32,32 @@ class Rotate(pt.behaviour.Behaviour):
         if new_status != pt.common.Status.RUNNING:
             self.cmd_pub.publish(Twist())
 
+class RotateCPU(pt.behaviour.Behaviour):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def setup(self, **kwargs):
+        self.node = kwargs['node']
+        self.rotation_speed = self.node.get_parameter('recovery.rotation_speed').value
+
+        self.cmd_pub = self.node.create_publisher(Twist, '/cmd_vel', 10)
+
+        return True
+
+    def update(self):
+        high = Blackboard.storage.get("/temperature_high", None)
+        self.node.get_logger().info(f"[Rotate] /temperature_high = {high}")
+        if high:
+            msg = Twist()
+            msg.angular.z = -1 * self.rotation_speed
+            self.cmd_pub.publish(msg)
+            return pt.common.Status.RUNNING
+        return pt.common.Status.SUCCESS
+    
+    def terminate(self, new_status):
+        if new_status != pt.common.Status.RUNNING:
+            self.cmd_pub.publish(Twist())
+    
 class StopMotion(pt.behaviour.Behaviour):
     def __init__(self, name):
         super().__init__(name)
@@ -70,10 +96,34 @@ class BatteryMonitor(pt.behaviour.Behaviour):
         return True
 
     def callback(self, msg):
-        low = msg.data < self.low_battery_threshold
+        low = msg.data <= self.low_battery_threshold
         self.blackboard.set("/battery_voltage", msg.data)
         self.blackboard.set("/battery_low", low)
         self.node.get_logger().info(f"[BatteryMonitor] /battery_voltage = {msg.data:.2f} | /battery_low = {low}")
+
+    def update(self):
+        return pt.common.Status.RUNNING
+
+class CPUMonitor(pt.behaviour.Behaviour):
+    def __init__(self, name):
+        super().__init__(name)
+        self.blackboard = Blackboard()
+        self.subscription = None
+
+    def setup(self, **kwargs):
+        self.node = kwargs['node']
+        
+        self.max_temperature_threshold = self.node.get_parameter('cpu.max_temperature_threshold').value
+        self.subscription = self.node.create_subscription(
+            Float32, '/cpu_temperature', self.callback, 10
+        )
+        return True
+
+    def callback(self, msg):
+        high = msg.data >= self.max_temperature_threshold 
+        self.blackboard.set("/cpu_temperature", msg.data)
+        self.blackboard.set("/temperature_high", high)
+        self.node.get_logger().info(f"[CPUMonitor] /cpu_temperature = {msg.data:.2f} | /temperature_high = {high}")
 
     def update(self):
         return pt.common.Status.RUNNING
